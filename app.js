4d860c3690ddc35e4a3e5f937815eb058629470a
tinn505 <martinlaraolivares@example.com>
2025-03-13 09:12:49 -0600

    foa -b grupos-y-estudiantes

---
+++ app.js
@@ -0,0 +1,28 @@
+// ### 2. Grupos y Estudiantes
+
+//  GET /grupos  
+//    Descripción: Obtener todos los grupos (para docentes o administradores).
+//    Respuesta: Lista de grupos.
+
+//  GET /grupos/{id}  
+//    Descripción: Obtener detalles de un grupo específico.
+//    Parámetros: id (ID del grupo).
+
+//  POST /grupos  
+//    Descripción: Crear un nuevo grupo (para administradores o docentes).
+//    Cuerpo: Detalles del grupo (nombre, docente, director, grado).
+
+//  GET /estudiantes/{id}  
+//    Descripción: Obtener la información de un estudiante específico.
+//    Parámetros: id (ID del estudiante).
+
+//  POST /estudiantes  
+//    Descripción: Registrar un nuevo estudiante.
+//    Cuerpo: Información del estudiante (nombre, apellidos, grupo, correo, etc.).
+
+//  GET /estudiantes/grupo/{id_grupo}  
+//    Descripción: Obtener todos los estudiantes de un grupo específico.
+//    Parámetros: id_grupo (ID del grupo).
+
+// 
+
