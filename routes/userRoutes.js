eef1f69a8d3879d1e50c546e39742fe487afe050
tinn505 <martinlaraolivares@example.com>
2025-03-26 01:04:46 -0600

    style: netoyer el debugeo

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const UserController = require('../controllers/userController');
 const  validarToken  = require('../middlewares/authMiddleware');
 
-console.log("verificarToken:", validarToken);
 // Registrar un nuevo usuario
 router.post('/register', UserController.registrar);
 
