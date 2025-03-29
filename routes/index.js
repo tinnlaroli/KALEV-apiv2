78742c1bf8f9da060f9245e4e15b13bd64cdb515
tinn505 <martinlaraolivares@example.com>
2025-03-29 04:20:37 -0600

    style format routes

--- routes/index.js
+++ routes/index.js
@@ -29,40 +29,6 @@ router.use('/estrategias_ensenanza', estrategiasRoutes);
 router.use('/historial', historialController);
 router.use('/', ionicMascotaAaronRoutes);
 router.use('/jugadores', jugadorRoutes);
-const express = require('express');
-const router = express.Router();
-
-// Importación de módulos de rutas
-const userRoutes = require('./userRoutes');
-const grupoRoutes = require('./gruposRoutes');
-const estudianteRoutes = require('./estudiantesRoutes');
-const actividadesRoutes = require('./actividadesRoutes');
-const juegosRoutes = require('./juegosRoutes');
-const metricasRoutes = require('./metricasRoutes');
-const sesionJuegosRoutes = require('./sesionJuegosRoutes');
-const comprasRoutes = require('./comprasRoutes');
-const itemsRoutes = require('./itemsRoutes');
-const estrategiasRoutes = require('./estrategiasRoutes');
-const historialController = require('./historialRoutes');
-const ionicMascotaAaronRoutes = require('./ionicMascotaAaronRoutes');
-const jugadorRoutes = require('./jugadorRoutes')
-
-router.use('/usuarios', userRoutes);
-router.use('/grupos', grupoRoutes);
-router.use('/estudiantes', estudianteRoutes);
-router.use('/actividades', actividadesRoutes);
-router.use('/juegos', juegosRoutes);
-router.use('/metricas', metricasRoutes);
-router.use('/sesiones_juego', sesionJuegosRoutes);
-router.use('/compras', comprasRoutes);
-router.use('/items', itemsRoutes);
-router.use('/estrategias_ensenanza', estrategiasRoutes);
-router.use('/historial', historialController);
-router.use('/', ionicMascotaAaronRoutes);
-router.use('/jugadores', jugadorRoutes);
-
-// Exportar el enrutador principal
-module.exports = router;
 
 // Exportar el enrutador principal
 module.exports = router;
